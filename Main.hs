4ced51f4e82688eaeb35270577a6bb82518c5d63
albinhsn <albin.henningsson1@gmail.com>
2023-08-18 17:12:44 +0200

    Some of the exercises for section 3

--- Main.hs
+++ Main.hs
@@ -3,18 +3,30 @@ module Main where
 import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Numeric 
 
 data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
              | Number Integer
+             | Character Char
              | String String
+             | Float Float
              | Bool Bool
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom 
+          <|> try parseFloat 
           <|> parseString
           <|> parseNumber
+          <|> parseCharacter
+
+parseFloat :: Parser LispVal
+parseFloat = do
+          x<- many1 digit
+          char '.'
+          y <- many1 digit
+          return $ Float (fst.head$readFloat(x ++ "." ++ y))
 
 parseAtom :: Parser LispVal
 parseAtom = do 
@@ -26,8 +38,28 @@ parseAtom = do
                         "#f" -> Bool False
                         _  -> Atom atom
 
+parseCharacter :: Parser LispVal
+parseCharacter = do 
+  try $ string "#\\"
+  value <- try (string "newline" <|> string "space") <|> 
+    do {
+      x <- anyChar; 
+      notFollowedBy alphaNum ; 
+      return $ case x of 
+        '\t' -> "\t"
+        '\n' -> "\n"
+        '\\' -> "\\"
+        otherwise -> [x]
+        }
+  return $ Character $ case value of 
+    "space" -> ' '
+    "newline" -> '\n'
+    otherwise -> (value!!0)
+
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = do 
+                x <- many1 digit
+                (return . Number . read) x
 
 parseString :: Parser LispVal
 parseString = do 
